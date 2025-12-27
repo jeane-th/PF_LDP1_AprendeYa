aeda21414ecd6e802b1d5cd2adf8335dfaacdf88
jordyeo2001-maker <jordyeo.2001@gmail.com>
2025-12-27 14:22:27 -0500

    Update Usuario.java

--- src/java/modelo/Usuario.java
+++ src/java/modelo/Usuario.java
@@ -11,20 +11,22 @@ package modelo;
 public class Usuario {
 
     // 1. ATRIBUTOS (Espejo de tu tabla en la imagen)
-    private int idUsuario;     
+    private int id_usuario;      // Viene de 'id_usuario'
     private String nombre;
     private String email;
     private String password;
     private String pais;
     private String rol;         // Nuevo campo que vi en tu imagen
-    private int estado;         // 1=Activo, 0=Inactivo
+    private int estado;
+    private int nroCursos;   
 
     // 2. CONSTRUCTOR VACÍO
     public Usuario() {
     }
 
-    public Usuario(int idUsuario, String nombre, String email, String password, String pais, String rol, int estado) {
-        this.idUsuario = idUsuario;
+    // 3. CONSTRUCTOR COMPLETO
+    public Usuario(int id_usuario, String nombre, String email, String password, String pais, String rol, int estado) {
+        this.id_usuario = id_usuario;
         this.nombre = nombre;
         this.email = email;
         this.password = password;
@@ -33,7 +35,7 @@ public class Usuario {
         this.estado = estado;
     }
 
-    public Usuario(String nombre, String email, String password, String pais, String rol, int estado) {
+    public Usuario(String nombre, String email, String password, String pais, String web, String descripcion, String rol, int estado) {
         this.nombre = nombre;
         this.email = email;
         this.password = password;
@@ -42,12 +44,13 @@ public class Usuario {
         this.estado = estado;
     }
 
-    public int getIdUsuario() {
-        return idUsuario;
+    // 4. GETTERS Y SETTERS
+    public int getId_usuario() {
+        return id_usuario;
     }
 
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
     }
 
     public String getNombre() {
@@ -74,14 +77,6 @@ public class Usuario {
         this.password = password;
     }
 
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
     public String getRol() {
         return rol;
     }
@@ -98,5 +93,22 @@ public class Usuario {
         this.estado = estado;
     }
 
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    public int getNroCursos() {
+        return nroCursos;
+    }
+
+    public void setNroCursos(int nroCursos) {
+        this.nroCursos = nroCursos;
+    }
 }
+
+
    
