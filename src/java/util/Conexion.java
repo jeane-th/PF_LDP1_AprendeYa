23a980098dff7cd9c5dc03d40119b59c04b6f89e
Jeaneth Tafur <jeanethtafur@gmail.com>
2026-01-20 09:50:05 -0500

    cambio de variables de la BD

--- src/java/util/Conexion.java
+++ src/java/util/Conexion.java
@@ -13,12 +13,14 @@ import java.sql.DriverManager;
  */
 public class Conexion {
 
-//    private static final String URL = System.getProperty("DB_URL");
-//    private static final String USER = System.getProperty("DB_USER");
-//    private static final String PASSWORD = System.getProperty("DB_PASS");
-    private static final String URL = "jdbc:mysql://bhofdycde3nrmo12f1zd-mysql.services.clever-cloud.com:3306/bhofdycde3nrmo12f1zd";
+
+    /*private static final String URL = "jdbc:mysql://bhofdycde3nrmo12f1zd-mysql.services.clever-cloud.com:3306/bhofdycde3nrmo12f1zd";
     private static final String USER = "uwerzwhyzlodeacv";
-    private static final String PASSWORD = "JUeJE6Aq6iyy0YrmYnxT";
+    private static final String PASSWORD = "JUeJE6Aq6iyy0YrmYnxT";*/
+
+    private static final String URL = System.getProperty("DB_URL");
+    private static final String USER = System.getProperty("DB_USER");
+    private static final String PASSWORD = System.getProperty("DB_PASS");
 
     public static Connection getConnection() {
         Connection con = null;
