96401e49862588f459af8314dab254e17a0c352f
Jeaneth Tafur <jeanethtafur@gmail.com>
2026-01-20 12:01:48 -0500

    prueba de BD

--- src/java/util/Conexion.java
+++ src/java/util/Conexion.java
@@ -13,7 +13,6 @@ import java.sql.DriverManager;
  */
 public class Conexion {
 
-
     private static final String URL = System.getenv("DB_URL");
     private static final String USER = System.getenv("DB_USER");
     private static final String PASSWORD = System.getenv("DB_PASS");
@@ -25,6 +24,8 @@ public class Conexion {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Se conectó a la base de datos");
+            System.out.println("URL = " + URL);
+            System.out.println("USER = " + USER);
         } catch (Exception ex) {
             System.out.println("Error de conexión: " + ex.getMessage());
         }
